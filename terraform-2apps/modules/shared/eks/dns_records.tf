5598a14331eed6f4230c7d1501621c4e30e30edd
mrste148 <pavluktu@gmail.com>
2026-04-03 20:55:15 +0300

    fix(dns):INIT-161 fix hardcoded namaspace

--- terraform-2apps/modules/shared/eks/dns_records.tf
+++ terraform-2apps/modules/shared/eks/dns_records.tf
@@ -1,7 +1,7 @@
 data "kubernetes_resources" "envoy_lb" {
   api_version    = "v1"
   kind           = "Service"
-  namespace      = "envoy-gateway-system"
+  namespace      = kubernetes_namespace_v1.envoy_gw_api.metadata[0].name
   label_selector = "gateway.envoyproxy.io/owning-gateway-name=buried-marks-gateway"
   depends_on     = [helm_release.envoy_gw_api]
 }
