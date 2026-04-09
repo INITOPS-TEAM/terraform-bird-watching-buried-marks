8c500b0bf0ccde8927be3dd2537625e54e33b8c0
mrste148 <pavluktu@gmail.com>
2026-04-09 20:21:33 +0300

    feat:INIT-171 Added prevent destroy for Jenkins

--- terraform-2apps/environments/stage/destroy.sh
+++ terraform-2apps/environments/stage/destroy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 terraform destroy \
-#    -target=module.birdwatching.aws_instance.lb \
-#    -target=module.birdwatching.aws_instance.app \
-#    -target=module.birdwatching.aws_instance.db \
-#    -target=module.birdwatching.aws_instance.consul \
-#    -target=module.vpc.aws_nat_gateway.this \
-#    -target=module.vpc.aws_eip.nat \
+    -target=module.birdwatching.aws_instance.lb \
+    -target=module.birdwatching.aws_instance.app \
+    -target=module.birdwatching.aws_instance.db \
+    -target=module.birdwatching.aws_instance.consul \
+    -target=module.vpc.aws_nat_gateway.this \
+    -target=module.vpc.aws_eip.nat \
     -target=module.landing \
     -target=module.eks
